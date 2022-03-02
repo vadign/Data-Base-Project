94244e194c167a1436e6cfa8341c3b80163b152e
vadign <71590473+vadign@users.noreply.github.com>
2022-03-02 22:10:38 +0700

    + HelloController

--- src/main/java/com/vadign/databaseproject/HelloController.java
+++ src/main/java/com/vadign/databaseproject/HelloController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
     @GetMapping("/Hello")
-    public String getHello(){
+    public String getHello() {
         return """
                 <!DOCTYPE html>
                 <html lang="en">
